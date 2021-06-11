6243af3536d036e697533e16e114c570dcfa634b
Victor de Souza <83593281+VictorTheSouza@users.noreply.github.com>
2021-06-11 13:41:38 -0300

    Add files via upload

--- BlogPessoal/src/main/java/org/generation/blogpessoal/configuration/SwaggerConfig.java
+++ BlogPessoal/src/main/java/org/generation/blogpessoal/configuration/SwaggerConfig.java
@@ -20,7 +20,7 @@ public class SwaggerConfig {
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("br.org.generation.blogpessoal.controller"))
+				.apis(RequestHandlerSelectors.basePackage("org.generation.blogpessoal.controller"))
 				.paths(PathSelectors.any()).build().apiInfo(metadata()).useDefaultResponseMessages(false)
 				.globalResponses(HttpMethod.GET, responseMessageForGET());
 	}
