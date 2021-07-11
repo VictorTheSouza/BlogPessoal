c238d4b0db5271581f484bcb520bbc9d7d07efe0
Victor de Souza <83593281+VictorTheSouza@users.noreply.github.com>
2021-07-11 20:11:57 -0300

    Update BasicSecurityConfig.java

--- BlogPessoal/src/main/java/org/generation/blogpessoal/seguranca/BasicSecurityConfig.java
+++ BlogPessoal/src/main/java/org/generation/blogpessoal/seguranca/BasicSecurityConfig.java
@@ -35,16 +35,17 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/**").permitAll()
-								.antMatchers("/usuarios/logar").permitAll()
-								.antMatchers("/usuarios/cadastrar").permitAll()
-								.antMatchers(HttpMethod.GET, "/postagens").permitAll()
-								.antMatchers(HttpMethod.GET, "/tema").permitAll()
-								.anyRequest().authenticated()
-								.and().httpBasic()
-								.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-								.and().cors()
-								.and().csrf().disable();
+		http.authorizeRequests()
+		.antMatchers("/**").permitAll()
+		.antMatchers("/usuarios/logar").permitAll()
+		.antMatchers("/usuarios/cadastrar").permitAll()
+		.antMatchers(HttpMethod.GET, "/postagens").permitAll()
+		.antMatchers(HttpMethod.GET, "/tema").permitAll()
+		.anyRequest().authenticated()
+		.and().httpBasic()
+		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and().cors()
+		.and().csrf().disable();
 		
 		
 	}
